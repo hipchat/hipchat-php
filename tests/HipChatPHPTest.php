09eaa78781ffce3422c203f728386f6c3a11b706
Garret Heaton <powdahound@gmail.com>
2012-11-12 21:15:13 +0000

    Update tests to proper namespace

--- tests/HipChatPHPTest.php
+++ tests/HipChatPHPTest.php
@@ -12,31 +12,31 @@ require_once 'PHPUnit/Framework.php';
 
 class HipChatPHPTest extends PHPUnit_Framework_TestCase {
 
-  private $target = 'http://api.hipchat.com';
+  private $target = 'https://api.hipchat.com';
 
   public function testBadToken() {
-    $hc = new HipChat('hipchat-php-test-bad-token', $this->target);
-    $this->setExpectedException('HipChat_Exception');
+    $hc = new HipChat\HipChat('hipchat-php-test-bad-token', $this->target);
+    $this->setExpectedException('HipChat\HipChat_Exception');
     $hc->get_rooms();
   }
 
   public function testBadTargetHost() {
     $bad_target = 'http://does-not-exist.hipchat.com';
-    $hc = new HipChat('hipchat-php-test-token', $bad_target);
-    $this->setExpectedException('HipChat_Exception');
+    $hc = new HipChat\HipChat('hipchat-php-test-token', $bad_target);
+    $this->setExpectedException('HipChat\HipChat_Exception');
     $hc->get_rooms();
   }
 
   public function testBadApiMethod() {
-    $hc = new HipChat('hipchat-php-test-token', $this->target);
-    $this->setExpectedException('HipChat_Exception');
+    $hc = new HipChat\HipChat('hipchat-php-test-token', $this->target);
+    $this->setExpectedException('HipChat\HipChat_Exception');
     $hc->make_request('bad/method');
   }
 
   public function testBadColor()
   {
     $bad_color = 'fancy pink';
-    $hc = new HipChat('hipchat-php-test-token', $this->target);
+    $hc = new HipChat\HipChat('hipchat-php-test-token', $this->target);
     $this->setExpectedException('InvalidArgumentException');
     $hc->message_room(1337, 'Hipchat', "Hi everybody.", false, $bad_color);
   }
